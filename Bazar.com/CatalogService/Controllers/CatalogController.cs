98374873313bbc22e28217a2b38b6a95ac6a47ca
Mohammad Aburihan <mohammadre1654@gmail.com>
2025-04-12 00:10:33 +0300

    edit catalog controller

--- Bazar.com/CatalogService/Controllers/CatalogController.cs
+++ Bazar.com/CatalogService/Controllers/CatalogController.cs
@@ -2,59 +2,51 @@
 using CatalogServer.Services;
 using Microsoft.AspNetCore.Mvc;
 
-namespace CatalogServer.Controllers
-{
-    [ApiController]
-    [Route("[controller]")]
-    public class CatalogController : ControllerBase
-    {
-        private readonly BookRepository _bookRepository;
-        public CatalogController(BookRepository bookRepository)
-        {
-            _bookRepository = bookRepository;
-        }
+namespace CatalogServer.Controllers {
+[ApiController]
+[Route("[controller]")]
+public class CatalogController : ControllerBase {
+  private readonly BookRepository _bookRepository;
+  public CatalogController(BookRepository bookRepository) {
+    _bookRepository = bookRepository;
+  }
 
-        [HttpGet("search/{topic}")]
-        public IActionResult Search(string topic)
-        {
-            var books = _bookRepository.GetByTopic(topic).Select(b => new { b.Id, b.Title });
-            return Ok(books);
-        }
+  [HttpGet("search/{topic}")]
+  public IActionResult Search(string topic) {
+    var books =
+        _bookRepository.GetByTopic(topic).Select(b => new { b.Id, b.Title });
+    return Ok(books);
+  }
 
-        [HttpGet("info/{id}")]
-        public IActionResult Info(int id)
-        {
-            var book = _bookRepository.GetById(id);
-            if (book == null)
-            {
-                return NotFound();
-            }
-            return Ok(new { book.Id ,book.Title, book.Quantity, book.Price });
-        }
-        [HttpGet("info")]
-        public IActionResult GetAllBooks()
-        {
-            var books = _bookRepository.GetAll();
-            if (books == null)
-            {
-                return NotFound();
-            }
-            return Ok(books);
-        }
-        [HttpPost("update/decrement/{id}")]
-        public IActionResult Update(int id)
-        {
-            var book = _bookRepository.GetById(id);
-            if (book == null)
-                return NotFound();
-            Console.WriteLine($"quan {book.Quantity}");
-            if (book.Quantity <= 0)
-                return BadRequest("Out of stock");
+  [HttpGet("info/{id}")]
+  public IActionResult Info(int id) {
+    var book = _bookRepository.GetById(id);
+    if (book != null) {
+      return Ok(new { book.Id, book.Title, book.Quantity, book.Price });
+    } else
+      return NotFound();
+  }
+  [HttpGet("info")]
+  public IActionResult GetAllBooks() {
+    var books = _bookRepository.GetAll();
+    if (books != null) {
+      return Ok(books);
+    } else
+      return NotFound();
+  }
+  [HttpPost("update/decrement/{id}")]
+  public IActionResult Update(int id) {
+    var book = _bookRepository.GetById(id);
+    if (book == null)
+      return NotFound();
+    Console.WriteLine($"quan {book.Quantity}");
+    if (book.Quantity < 1)
+      return BadRequest("Out of stock");
 
-            book.Quantity--;
-            _bookRepository.Update(book);
+    book.Quantity--;
+    _bookRepository.Update(book);
 
-            return Ok(book);
-        }
-    }
+    return Ok(book);
+  }
+}
 }
